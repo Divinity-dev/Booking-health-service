4c0201e4071793f12e9439693b6fe65b5624806f
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-06-29 09:27:26 +0100

    style

--- src/routes/UnAuthenticatedRoute.jsx
+++ src/routes/UnAuthenticatedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LoginForm from '../pages/Login/login';
+import LoginForm from '../pages/Login/Login';
 import SignupForm from '../pages/signup/signup';
 
 const UnAuthenticatedRoute = () => (
